da3469333208102cbf062d31d2f3a3c68bb1deb7
sathwiksharma2206 <deshapathi.sathwik@kalvium.community>
2025-04-28 09:40:04 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require("express")
+const app =express()
+app.use(express.json())
+PORT=3000;
+ 
+const users={
+{(email="joe@example.com", password="joe123")}
+{
+    if (!email===(req,res)  )
+}
+}
